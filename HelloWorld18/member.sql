1b2d99627f8778e9d5b243779896396149b7a193
NamepeN <maminang@gmali.com>
2019-03-28 10:45:20 +0900

    db작업중 중간커밋

--- HelloWorld18/member.sql
+++ HelloWorld18/member.sql
@@ -1,34 +1,30 @@
-iNo
 id
+pw
 name
-birth
 e-mail
 address
 phone
 point
 가입한 날짜
-찜
-구매내역
-장바구니
+
+iNo - id가 기본키역할을 할수 있으니까 뺏어요
 
 CREATE TABLE tbl_member(
-iNo number PRIMARY KEY,
-id varchar2(30) not null,
+id varchar2(30) PRIMARY KEY,
+pw varchar2(30) not null,
 name varchar2(30) not null,
-birth date not null,
-email varchar2(30) not null,
-address varchar2(30),
+email varchar2(150) not null,
+address varchar2(150),
 phone number,
-point number,
+birth date not null,
+point number default 0,
 signUpDate date default sysdate
 )
 
-CREATE TABLE tbl_cart(
-column1 DATE_TYPE1 PRIMARY KEY,
-column2 DATE_TYPE2,
-column3 DATE_TYPE3,
-column4 DATE_TYPE4
-)
+insert into tbl_member (id,pw,name,email,address,phone,birth) values ('NamepeN', 'qlalfqjsgh', '임하민', 'maminang@gmail.com','서울특별시 성북구 장위동 111-111 중앙빌라 111호', 010-2334-4290, '1996-09-06')
+insert into tbl_member (id,pw,name,email,address,phone) values ('maminang', 'qlalfqjsgh', '임하민', 'maminang@gmail.com','서울특별시 성북구 장월로 11길 11-1 중앙빌라 112호', 010-2334-4290, '2019-03-28')
+
+select * from tbl_member
 
 drop table tbl_member
 
---
+++ HelloWorld18/product.sql
@@ -0,0 +1,39 @@
+pno 기본키
+eng_name 제품 영어이름
+kr_name 제품 한글이름
+prdct_dscrp(PRODUCT_DESCRIPTIONS) 제품 소개
+volume 용량
+price 가격
+category 카테고리
+
+create table tbl_product (
+	pno number primary key,
+	eng_name varchar2(100),
+	kr_name varchar2(100),
+	prdct_dscrp varchar2(600),
+	volume number,
+	price number,
+	category varchar2(100) 
+)
+
+drop table tbl_product
+
+insert into tbl_product values (
+1,
+'Lime Basil & Mandarin Cologne',
+'라임 바질 앤 만다린 코롱',
+'조 말론 런던의 시그니처 향. 열정적인 허브의 독특한 조합으로 이루어져 있습니다. 카리브해의 산들바람에서 실려온 듯한 라임향에 톡 쏘는 바질과 향기로운 백리향이 더해져서 독특한 조합을 만들어냅니다. 현대적인 감각의 클래식한 향입니다.',
+30,
+93000,
+'citrus'
+)
+
+insert into tbl_product values (
+2,
+'Lime Basil & Mandarin Cologne',
+'라임 바질 앤 만다린 코롱',
+'조 말론 런던의 시그니처 향. 열정적인 허브의 독특한 조합으로 이루어져 있습니다. 카리브해의 산들바람에서 실려온 듯한 라임향에 톡 쏘는 바질과 향기로운 백리향이 더해져서 독특한 조합을 만들어냅니다. 현대적인 감각의 클래식한 향입니다.',
+100,
+186000,
+'citrus'
+)
